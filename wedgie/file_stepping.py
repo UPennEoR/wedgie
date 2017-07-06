5edbf6bd14626c661d7ddb780d2ad188ac8c0f7b
Austin Fox Fortino <fortino@sas.upenn.edu>
2017-07-06 13:04:09 -0400

    added hardcoded directory on folio2

--- wedgie/file_stepping.py
+++ wedgie/file_stepping.py
@@ -13,15 +13,15 @@ Last Updated: July 5, 2017
 """
 
 from os import system, listdir
+from glob import glob
 from pprint import pprint
 from time import time
 
 t0 = time()
 
 files = []
-for file in listdir("../test_data"):
-    if "zen" not in file: continue
-    files.append("../test_data/" + file)
+for file in glob("/data4/paper/HERA2015/2457746/PennData/fourpol/*.OR"):
+    files.append("/data4/paper/HERA2015/2457746/PennData/fourpol/*.OR" + file)
 sorted(files)
 
 calfile = "hsa7458_v001"
