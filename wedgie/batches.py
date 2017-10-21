6ec0a4d85b865ec348f7050d358c38bb9c738206
Austin Fox Fortino <fortino@sas.upenn.edu>
2017-10-21 17:33:28 -0400

    added compatibility for python2.7

--- wedgie/batches.py
+++ wedgie/batches.py
@@ -1,9 +1,11 @@
 import os
 
 days = ['2457548', '2457549', '2457550', '2457551', '2457552', '2457553', '2457554', '2457555']
+rng = '580_680'
 
 for day in days:
-    os.system(f"python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/{day}/*RO -X=81,22,43 -R={rng} -t")
-    folder = f"{day}_{rng}"
+    os.system("python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/{day}/*RO -X=81,22,43 -R={rng} -t".format(
+        day=day, rng=rng))
+    folder = "{day}_{rng}".format(day=day, rng=rng)
     os.mkdir(folder)
-    os.system(f"mv *{day}* {folder}")
+    os.system("mv *{day}* {folder}".format(day=day, rng=rng))
