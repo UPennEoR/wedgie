c4ead87d55406ed1bd42de074291703d9dfabe55
Austin Fox Fortino <fortino@sas.upenn.edu>
2017-10-21 23:33:58 -0400

    updated batch script after reading up on qsub

--- wedgie/batches.sh
+++ wedgie/batches.sh
@@ -1,5 +1,11 @@
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*RO -X=81,22,43 -R=580_680 -t
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*RO -X=81,22,43 -R=580_680 -t
+#!/bin/bash
+#$ -N 7days_hi_band
+#$ -l h_vmem=40G
+#$ -cwd
+#$ -m ea
+
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*RO -X=81,22,43 -R=580_680 -t
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457550/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457551/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457552/*RO -X=81,22,43 -R=580_680 -t
