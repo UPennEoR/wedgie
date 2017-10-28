6b055ee38180940bf01fb5722fb4aa302117e2a9
Austin Fox Fortino <fortino@sas.upenn.edu>
2017-10-27 21:00:48 -0400

    updated batch script to hopefully actually work

--- wedgie/batches.sh
+++ wedgie/batches.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#$ -N 7days_hi_band
-#$ -l h_vmem=40G
+#$ -N HolyDaysHigh
+#$ -l h_vmem=45G
 #$ -cwd
-#$ -m ea
+#$ -m bea
+#$ -V
 
-# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*RO -X=81,22,43 -R=580_680 -t
-# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*RO -X=81,22,43 -R=580_680 -t
+source activate hera
+python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*RO -X=81,22,43 -R=580_680 -t
+python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457550/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457551/*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457552/*RO -X=81,22,43 -R=580_680 -t
