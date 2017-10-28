dce17418f8a7b1899c9cd54b90f706377a59b5bf
Austin Fox Fortino <fortino@sas.upenn.edu>
2017-10-28 18:45:15 -0400

    updated batch scripts

--- wedgie/batches_hi.sh
+++ wedgie/batches_hi.sh
@@ -6,11 +6,11 @@
 #$ -V
 
 source activate hera
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457548/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457549/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457550/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457551/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457552/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
 python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457553/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457554/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
-python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457555/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457554/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
+# python2.7 getWedge.py -F /data4/paper/plaplant/HERA2015/upenn_projects/2457555/*.[3-6]*RO -X=81,22,43 -R=580_680 -t
