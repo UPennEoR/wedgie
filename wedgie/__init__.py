048bd9e3b479c8e1f79e02e62900817612885a09
Paul La Plante <plaplant@sas.upenn.edu>
2017-11-27 12:06:46 -0800

    Make calfile directory import-able

--- wedgie/__init__.py
+++ wedgie/__init__.py
@@ -1,6 +1,12 @@
 """init file for wedgie"""
+import os
+import sys
 from .cosmo_utils import *
 from .gen_utils import *
 from .wedge_utils import *
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+CAL_DIR = os.path.join(ROOT, 'calibrations')
+sys.path.append(CAL_DIR)
 
 __version__ = 0.1
